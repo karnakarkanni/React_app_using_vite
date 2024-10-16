a63f69ebb2615281311bb636b76999844441193d
karnakarmarati <karnakarmaratikanni@gmail.com>
2024-10-16 14:26:07 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
     <>
          <div className="App">
       <h2>MARATI KARNAKAR</h2>
-      <h4>Email:<a href="">karnakarmaratikanni@gmail.com</a> </h4>
-      <h4>Contact Number:<a href="">+916302948618</a> </h4>
-      <h4>Github:<a href=""> https://github.com/karnakarkanni</a> </h4>
-      <h4>Linkdini:<a href="">https://www.linkedin.com/in/karnakar-marati-644097257/</a> </h4>
+      <h4>Email:<a href="karnakarmaratikanni@gmail.com">karnakarmaratikanni@gmail.com</a> </h4>
+      <h4>Contact Number:<a href="+916302948618">+916302948618</a> </h4>
+      <h4>Github:<a href=" https://github.com/karnakarkanni"> https://github.com/karnakarkanni</a> </h4>
+      <h4>Linkdini:<a href="https://www.linkedin.com/in/karnakar-marati-644097257/">https://www.linkedin.com/in/karnakar-marati-644097257/</a> </h4>
       <hr></hr>
 
     </div>
